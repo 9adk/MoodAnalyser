fadeba27a2e0335ba0a81bd79641d78572a5651b
aditya Kharade <adityakharade51@gmail.com>
2020-10-05 18:01:31 +0530

    Added : added analyseMood functionality

---
+++ src/main/java/com/moodAnalyser/App.java
@@ -0,0 +1,13 @@
+package com.moodAnalyser;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
