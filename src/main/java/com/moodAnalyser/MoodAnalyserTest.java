b816f56db7bc4afe86e7f87539e5c9d5744af1a4
aditya Kharade <adityakharade51@gmail.com>
2020-10-05 18:06:28 +0530

    Added : added test cases to validate mood

--- src/main/java/com/moodAnalyser/MoodAnalyserTest.java
+++ src/main/java/com/moodAnalyser/MoodAnalyserTest.java
@@ -7,7 +7,14 @@ import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
 class MoodAnalyserTest {
-		
-
-
+		@Test
+		void givenMessage_whenSad_thenReturnSadMood() {
+			MoodAnalyser moodAnalyser = new MoodAnalyser();
+			Assert.assertEquals("SAD", moodAnalyser.analyseMood("I am in sad mood"));
+		}
+		@Test
+		void givenMessage_whenAny_thenReturnSadMood() {
+			MoodAnalyser moodAnalyser = new MoodAnalyser();
+			Assert.assertEquals("HAPPY", moodAnalyser.analyseMood("I am in any mood"));
+		}
 }
